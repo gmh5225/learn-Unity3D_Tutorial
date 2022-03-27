f92b033c8b3e32bf2d8768bdbcfa34a3b4f59251
ebadier <emmanuel.badier@gmail.com>
2022-03-27 14:16:45 +0200

    - CameraMovement ready for course 007

--- Assets/Game_Tutorial/02_Player/Scripts/CameraMovement.cs
+++ Assets/Game_Tutorial/02_Player/Scripts/CameraMovement.cs
@@ -16,64 +16,67 @@
 * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.							  *
 ******************************************************************************************************************************************************/
 
-#define CAM_CORRECTION
-
 using UnityEngine;
 
 // Put this script on the Camera's gameObject
 public class CameraMovement : MonoBehaviour
 {
-	public CharacterMovement characterMovement;
-	// Exercise 01: Use Mouse X Axis to control character's yaw rotation.
-	//				Create a public float variable yawSpeed to adjust the yaw rotation speed if needed.
-	// Exercise 02: Yaw rotation should consider character state (grounded or not) to adapt the yaw rotation speed.
+	// Exercise 01: Ensure Camera is placed at a convenient position & rotation at start.
+	//				Create a public CharacterMovement variable "characterMovement" to access characterMovement.transform.
+	// Exercise 02: Use Mouse X Axis to control character's yaw rotation.
+	//				Create a public float variable "yawSpeed" to adjust the yaw rotation speed if needed.
+	// Exercise 03: Yaw rotation should consider character state (grounded or not) to adapt the yaw rotation speed.
 	//				Allow characterMovement to share its _isGrounded attribute using a public readonly property "IsGrounded".
 	//				Use the characterMovement.airAngularControl to adjust the yaw rotation speed.
-	// Exercise 03: Use Mouse Y axis to control camera's pitch rotation.
-	//				Create a public float variable pitchSpeed to adjust the pitch rotation speed if needed.
+	// Exercise 04: Use Mouse Y axis to control camera's pitch rotation.
+	//				Create a public float variable "pitchSpeed" to adjust the pitch rotation speed if needed.
 	//				Camera's pitch rotation should also consider character state (grounded or not) to adapt the pitch rotation speed.
 	//				Use the characterMovement.airAngularControl to adjust the yaw rotation speed.
-	// Exercise 04: Where can we put the code to update CameraMovement to be sure it will always take into account the latest character's state ?
+	// Exercise 05: Where can we put the code to update CameraMovement to be sure it will always take into account the latest character's state ?
 	//				https://docs.unity3d.com/ScriptReference/MonoBehaviour.LateUpdate.html
-	// Exercise 05: What problem do you have when camera's pitch > 90f or < -90f ?
+	// Exercise 06: What problem do you have when camera's pitch > 90f or < -90f ?
 	//				What can you do to tackle this issue ?
 	//				Constraint pitch values to a comfortable range for the gameplay.
-	// Exercise 06: - If the script is enabled : the mouse cursor should disappear & be locked to the center of the screen + characterMovement.strafe should be true
+	// Exercise 07: - If the script is enabled : the mouse cursor should disappear & be locked to the center of the screen + characterMovement.strafe should be true
 	//				- If the script is disabled : the mouse cursor should appear & be unlocked + characterMovement.strafe should be false
 	//				https://docs.unity3d.com/ScriptReference/Cursor.html
-	// Exercise 07: The second stick of a gamepad can also be used instead of Mouse X/Y axis.
+	// Exercise 08: The second stick of a gamepad can also be used instead of Mouse X/Y axis.
 	//				- Create a second "Mouse X" axis (if not already existing) : Gravity = 0 ; Dead = 0.3 ; Sensitivity = 1 ; Type = Joystick Axis ; Axis = 4th axis
 	//				- Create a second "Mouse Y" axis (if not already existing) : Gravity = 0 ; Dead = 0.3 ; Sensitivity = 1 ; Invert = true ; Type = Joystick Axis ; Axis = 5th axis
-	// Exercise 08: Allow the use of the mouse scrollwheel to control camera distance from the character (zoom in/out)
+	// Exercise 09: Allow the use of the mouse scrollwheel to control camera distance from the character (zoom in/out)
 	//				Create a public float variable "mouseWheelSpeed" to adjust the zoom in/out speed.
-	// Exercise 09: Constraint camera distance to vary in a comfortable range for the gameplay
-	//				- Create a public float variable minDistance
-	//				- Create a public float variable maxDistance
-	// Exercise 10:	Camera distance should be also controllable with a gamepad
+	//				Along which vector do you need to move the camera ?
+	//				Create a private Vector3 variable "_characterToCameraDir" to adjust the zoom in/out speed.
+	//				Ensure that this vector is never equal to Vector3.zero.
+	// Exercise 10: Constraint camera distance to vary in a comfortable range for the gameplay
+	//				- Create a public float variable "minDistance"
+	//				- Create a public float variable "maxDistance"
+	// Exercise 11:	Camera distance should be also controllable with a gamepad
 	//				- Create a second "Mouse ScrollWheel" axis (if not already existing) : Gravity = 0 ; Dead = 0.01 ; Sensitivity = 0.1 ; Type = Joystick Axis ; Axis = 3rd axis
-	// Exercise 11: Create string constants on the top of the script to replace easily all Axis used in your code.
+	// Exercise 12: Create string constants on the top of the script to replace easily all Axis used in your code.
 
-#if CAM_CORRECTION
-	// Exercise 11:
+	// Exercise 12
 	public const string MOUSE_X = "Mouse X";
 	public const string MOUSE_Y = "Mouse Y";
 	public const string MOUSE_SW = "Mouse ScrollWheel";
 
-	// Exercise 01:
+	// Exercise 01
+	public CharacterMovement characterMovement;
+	// Exercise 02
 	public float yawSpeed = 1f;
-	// Exercise 03:
+	// Exercise 04
 	public float pitchSpeed = 1f;
-	// Exercise 05: Camera Pitch constrained
+	// Exercise 06 Camera Pitch constrained
 	public float pitchMin = -17f;
 	public float pitchMax = 23f;
-	// Exercise 08:
+	// Exercise 09
 	public float mouseWheelSpeed = 10f;
-	// Exercise 09:
+	// Exercise 10
 	public float minDistance = 10f;
 	public float maxDistance = 40f;
 
-	private Transform _cameraTransform;
-	private Transform _characterTransform;
+	// Exercise 09
+	private Vector3 _characterToCameraDir;
 
 	// Exercise 06: enabled = cursor disappear & is locked to the center of the screen + characterMovement.strafe = true
 	private void OnEnable()
@@ -92,45 +95,49 @@ public class CameraMovement : MonoBehaviour
 
 	private void Awake()
 	{
-		_cameraTransform = transform;
-		_characterTransform = characterMovement.transform;
+		// Exercise 01 : Ensure parent is character and a convenient placement of the camera is set at start.
+		transform.parent = characterMovement.transform; // As a child of this transform, the camera movement is following automatically this transform.
+		transform.localPosition = new Vector3(0f, 4f, -10f); // Set the camera a little higher than the character, and a little behind.
+		transform.localEulerAngles = new Vector3(15f, 0f, 0f); // Pitch the camera a little towards the ground.
+		
+		// Exercise 09 : Here we are sure the direction vector is not Vector3.zero.
+		// We normalize it once for all to avoid duplicate computations in Update().
+		_characterToCameraDir = transform.localPosition.normalized;
 	}
 
 	// Execute after Character movements
 	private void LateUpdate()
 	{
-		// Exercise 01:
+		// Exercise 02:
 		float yaw = Input.GetAxisRaw(MOUSE_X) * yawSpeed;
-		// Exercise 03:
+		// Exercise 04:
 		float pitch = -Input.GetAxisRaw(MOUSE_Y) * pitchSpeed;
 
 		if(!characterMovement.IsGrounded)
 		{
-			// Exercise 02: adapt yaw regarding character's grounded state
+			// Exercise 03: adapt yaw regarding character's grounded state
 			yaw *= characterMovement.airAngularControl;
-			// Exercise 03: adapt pitch regarding character's grounded state
+			// Exercise 04: adapt pitch regarding character's grounded state
 			pitch *= characterMovement.airAngularControl;
 		}
-		// Exercise 01: Character Yaw
-		_characterTransform.Rotate(Vector3.up, yaw, Space.Self);
-		// Exercise 03: Camera Pitch
-		Vector3 euler = _cameraTransform.localEulerAngles;
+		// Exercise 02: Character Yaw
+		characterMovement.transform.Rotate(Vector3.up, yaw, Space.Self);
+		// Exercise 04: Camera Pitch
+		Vector3 euler = transform.localEulerAngles;
 		//euler.x += _cameraPitch;
-		// Exercise 05: Camera Pitch constrained
+		// Exercise 06: Camera Pitch constrained
 		euler.x = _ClampAngle(euler.x + pitch, pitchMin, pitchMax);
-		_cameraTransform.localEulerAngles = euler;
-		// Exercise 08: Mouse wheel controls camera Distance
+		transform.localEulerAngles = euler;
+		// Exercise 09: Mouse wheel controls camera Distance
 		float deltaDistance = -Input.GetAxisRaw(MOUSE_SW) * mouseWheelSpeed;
-		Vector3 cameraOffset = _cameraTransform.localPosition;
-		//cameraOffset += cameraOffset.normalized * mouseWheel;
-		//_cameraTransform.localPosition = cameraOffset;
-		// Exercise 09: Constraint camera Distance
-		float distance = cameraOffset.magnitude;
+		//transform.localPosition += _characterToCameraDir * deltaDistance;
+		// Exercise 10: Constraint camera Distance
+		float distance = transform.localPosition.magnitude;
 		float newDistance = Mathf.Clamp(distance + deltaDistance, minDistance, maxDistance);
-		_cameraTransform.localPosition = cameraOffset / distance * newDistance;
+		transform.localPosition = _characterToCameraDir * newDistance;
 	}
 
-	// Exercise 05: 
+	// Exercise 06
 	private float _ClampAngle(float pAngle, float pMinAngle = -180f, float pMaxAngle = 180f)
 	{
 		// Ensure angle is in [-180;180]
@@ -142,5 +149,4 @@ public class CameraMovement : MonoBehaviour
 		// Clamp
 		return pAngle > pMaxAngle ? pMaxAngle : pAngle < pMinAngle ? pMinAngle : pAngle;
 	}
-#endif
 }
